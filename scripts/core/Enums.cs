844eed1724b77a76454907a29900ae01d7e6b3a0
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-07-17 01:13:50 +0200

    Add character movement and animation components

--- scripts/core/Enums.cs
+++ scripts/core/Enums.cs
@@ -7,3 +7,20 @@ public enum LogLevel
     WARNING,
     ERROR,
 }
+
+public enum ECharacterAnimation
+{
+  idle_down,
+  idle_up,
+  idle_left,
+  idle_right,
+  walk_down,
+  walk_up,
+  walk_left,
+  walk_right,
+  turn_down,
+  turn_up,
+  turn_left,
+  turn_right,
+  
+}
